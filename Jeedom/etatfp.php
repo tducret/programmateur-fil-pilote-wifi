d2a0e52c6e65340270d914cafe26595260e00a17
Julien <julien@famille-garnier.com>
2015-04-14 12:36:37 +0200

    Utilisation de varibales Jeedom

--- Jeedom/etatfp.php
+++ Jeedom/etatfp.php
@@ -1,26 +1,37 @@
 #!/usr/bin/php
 <?php
-// Theju / 15/04/2015 / création du script
+// =========================================================================
+//  Theju / 15/04/2015 / création du script
+// =========================================================================
 
 // Script permettant de récuperer les infos des fils pilotes de la carte de Tibault
 // https://github.com/thibdct/programmateur-fil-pilote-wifi
 // Le script prend un argument, simplement le numéro du fil pilote.
 // Il est fourni tel quel, attention, aucun verification n'est faite sur les variable ou sur l'argument passé.
  
+// =========================================================================
 // Utilisation dans Jeedom : 
+// =========================================================================
+
+// Creer 2 variables (General / Scenarios / Voir Variables / Ajouter)
+// SPARK_ID : L'id de votre spark
+// SPARK_TOKEN : Votre token 
+//
 // Créer un script puis une commande d'info, type script, sous type numerique. 
-// Clisquer sur nouveau
+// Cliquer sur nouveau
 // Copier / Coller ce script
-// Modifier les variables $core et $token en fonction de votre spark core
 // Enregistrer
 // Le chemin du script apparait dans le champ requete, a jouter a la fin le numéro du fil pilote a cheker : 
 // ex Fil pilote 1 : /chemin vers le script/etatFP.php 1
-
-// Declaration des variables :
-$core = "votre core id";
-$token = "votre token";
+// =========================================================================
 
 // Plus rien a modifier en dessous de cette ligne.
+
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';  
+
+// Declaration des variables :
+$core = scenario::getData("SPARK_ID");
+$token = scenario::getData("SPARK_TOKEN");
 
 // Let's Go !
 //recuperation de l'argument du cFP
